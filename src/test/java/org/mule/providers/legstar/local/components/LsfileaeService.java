6e2090dc3fa14face51948c8d317e4290e947f7d
fady <fady@6810f1fe-32b5-efba-0c3f-87fbb5f72bdc>
2008-07-17 07:06:08 +0000

    Rearchitected to separate the generation tools from the transport itself. The tools are now in a subfolder called tools, different from src. There is a separate POM for the tools.

--- src/test/java/org/mule/providers/legstar/local/components/LsfileaeService.java
+++
@@ -1,8 +0,0 @@
-package org.mule.providers.legstar.local.components;
-
-import com.legstar.test.coxb.lsfileae.DfhcommareaType;
-
-public interface LsfileaeService {
-    public DfhcommareaType lsfileae(DfhcommareaType request);
-
-}
--- src/test/java/org/mule/providers/legstar/local/components/LsfilealService.java
+++
@@ -1,9 +0,0 @@
-package org.mule.providers.legstar.local.components;
-
-import com.legstar.test.coxb.lsfileal.ReplyDataType;
-import com.legstar.test.coxb.lsfileal.RequestParmsType;
-
-public interface LsfilealService {
-    public ReplyDataType lsfileal(RequestParmsType request);
-
-}
