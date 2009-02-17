b56d96cd70f194bdffce275c0099d05327be76a6
fady <fady@6810f1fe-32b5-efba-0c3f-87fbb5f72bdc>
2009-02-17 17:24:42 +0000

    Updated copyright info in source code and added maven site descriptors.

--- legstar-mule-transport/src/main/java/org/mule/providers/legstar/i18n/LegstarMessages.java
+++ legstar-mule-transport/src/main/java/org/mule/providers/legstar/i18n/LegstarMessages.java
@@ -1,11 +1,12 @@
 /*******************************************************************************
- * $Id$
- * -----------------------------------------------------------------------------
- * Copyright (c) MuleSource, Inc. All rights reserved. http://www.mulesource.com
+ * Copyright (c) 2009 LegSem.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file
+ * Contributors:
+ *     LegSem - initial API and implementation
  ******************************************************************************/
 package org.mule.providers.legstar.i18n;
 
