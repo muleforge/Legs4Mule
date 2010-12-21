c3ea6c9703de947c50393cd6fe5eb13c9e2cfa1c
fady <fady@6810f1fe-32b5-efba-0c3f-87fbb5f72bdc>
2010-12-21 14:26:25 +0000

    Automate publishing of P2 artifacts.

--- legstar-mulegen-site/src/main/cmd/publish.bat
+++
@@ -1,7 +0,0 @@
-java -jar "%ECLIPSE_HOME%/plugins/org.eclipse.equinox.launcher_1.0.200.v20090520.jar" ^
--application org.eclipse.equinox.p2.publisher.UpdateSitePublisher ^
--metadataRepository file:/D:\Legsem\Legstar\mule\legstar-mule\mule-2.0.x\legstar-mulegen-site ^
--artifactRepository file:/D:\Legsem\Legstar\mule\legstar-mule\mule-2.0.x\legstar-mulegen-site ^
--source /D:\Legsem\Legstar\mule\legstar-mule\mule-2.0.x\legstar-mulegen-site ^
--compress  ^
--configs gtk.linux.x86
