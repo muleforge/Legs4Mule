a5b6f6dbff4303f161aae6584c7d449fe17d8519
fady <fady@6810f1fe-32b5-efba-0c3f-87fbb5f72bdc>
2011-01-02 07:17:48 +0000

    Changed the trunk to contain release 3.0.x

---
+++ legstar-mule-transport/src/test/java/org/mule/transport/legstar/http/LegstarHttpMessageAdapterTestCase.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2009 LegSem.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     LegSem - initial API and implementation
+ ******************************************************************************/
+package org.mule.transport.legstar.http;
+
+import org.mule.transport.AbstractMessageAdapterTestCase;
+import org.mule.api.transport.MessageAdapter;
+import org.mule.api.MessagingException;
+
+/**
+ * Test the LegstarHttpMessageAdapter class.
+ * For general guidelines on writing transports see http://mule.mulesource.org/display/MULE/Writing+Transports
+ */
+public class LegstarHttpMessageAdapterTestCase extends AbstractMessageAdapterTestCase {
+
+    /** {@inheritDoc} */
+    public Object getValidMessage() throws Exception {
+        return new String("mok message").getBytes();
+    }
+
+    /** {@inheritDoc} */
+    public MessageAdapter createAdapter(final Object payload) throws MessagingException {
+        return new LegstarHttpMessageAdapter(payload);
+    }
+
+}
